35e30138a0d29d62844e8fd19371495f32f502fe
root <3626@holbertonschool.com>
2021-09-28 12:44:52 -0700

    listing a-z with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+/**
+* main - printing alphabet in lower case with putchar
+* Return: nothing else
+*/
+#include<stdio.h>
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar (ch);
+	}
+	return (0);
+}

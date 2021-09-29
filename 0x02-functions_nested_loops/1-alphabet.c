896d4f22af7bbe418cf1bad9b8836ee5c9f866e6
root <3626@holbertonschool.com>
2021-09-29 07:54:56 -0700

    alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+*main - printout lowercase alphabet
+*Return: nothing else
+*/
+#include<stdio.h>
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	putchar(l);
+	putchar('\n');
+	return (0);
+}

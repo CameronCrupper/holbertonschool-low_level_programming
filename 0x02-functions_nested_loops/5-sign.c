e1a2a67cd4a67c6c8358c8033f2a827f549877bd
root <3626@holbertonschool.com>
2021-09-29 13:30:41 -0700

    printing out a sign for a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_sign - printing out a positive or negative for a number
+*@n: character to be described
+*Return: nothing else
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}

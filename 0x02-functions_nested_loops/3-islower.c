511f9283f545f492f62ff5628d865a34e171c6a3
root <3626@holbertonschool.com>
2021-09-29 12:33:45 -0700

    is letter lowercase or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,15 @@
+#include "main.h"
 /**
-*
-*
+*_islower - checking for lowercase
+*@c: lowercase
+*Return: nothing else
 */
-#include<stdio.h>
-int main(void)
+int _islower(int c)
 {
+	if (c >= 'a' && c >= 'z')
+		_islower(1);
+	else if (c >= 'A' && c >= 'Z')
+		_islower(0);
 
+	return (0);
 }

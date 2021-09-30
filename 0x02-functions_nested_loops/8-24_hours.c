ba811ea17e65812390dc129f6cb16ffb1377370b
root <3626@holbertonschool.com>
2021-09-29 21:53:56 -0700

    day clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*jack_bauer - printing every minute and hour of a day
+*Return- nothing else
+*/
+void jack_bauer(void)
+{
+	int h, m;
+
+	while (h < 24)
+	{
+		m = 0;
+			while (m < 60)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+				_putchar(':');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+				m++;
+			}
+		h++;
+	}
+}

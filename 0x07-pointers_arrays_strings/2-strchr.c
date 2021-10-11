fdf5cb6f40b3aede0f98e1fa200cbe0590164d22
root <3626@holbertonschool.com>
2021-10-11 13:23:08 -0700

    return end of string from specific value

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "strlen.c"
+/**
+ *_strchr - locate a specific character in a string
+ *@s: string
+ *@c: letter to start at
+ *Return: rest of string
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+		return (s);
+		}
+	s++;
+	}
+	return (s);
+}

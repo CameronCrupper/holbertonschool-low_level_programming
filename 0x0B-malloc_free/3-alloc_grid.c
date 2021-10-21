3c9cf0821b8a5d58308fa597e0d186236d13c8c3
root <3626@holbertonschool.com>
2021-10-20 20:52:58 -0700

    2 dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *free_grid - return a pointer to a 2 dimensional array
+ *@grid: 2 dimmensional array
+ *@height: columns
+ *Return: 2 dimensional array
+ */
+void free_grid(int **grid, int height)
+{
+	int z;
+	int n;
+	int w;
+
+	int (*10())[height]
+	{
+		int (*z)[n] = malloc(w * height * sizeof(int));
+		return (z);
+	}
+}

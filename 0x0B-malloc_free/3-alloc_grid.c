7a17001733e01c54c2c936d0eacdaa3fbe73dde8
root <3626@holbertonschool.com>
2021-10-20 21:17:40 -0700

    2 dimensional array grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *print_grid - prints a grid of integers
- *@grid: address of the 2 dimmensional grid
+ *alloc_grid - prints a grid of integers
  *@width: width of the grid
  *@height: height of the grid
+ *Return: nothing else
  */
 int **alloc_grid(int width, int height)
 {
-	
+	int **array;
+	int i;
+
+	array = malloc(width * sizeof(int *));
+	for (i = 0; i < width; i++)
+	{
+	array[i] = malloc(height * sizeof(int));
+	}
+return (0);
 }

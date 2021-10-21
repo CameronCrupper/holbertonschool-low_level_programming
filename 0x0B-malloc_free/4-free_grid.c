295db0d794cc732fd90228e97baf71162397a1f3
root <3626@holbertonschool.com>
2021-10-20 20:51:52 -0700

    free a 2 dimensional array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *free_grid - free a 2 dimensional array
+ *@grid: address of the two dimensional array
+ *@height: height of the grid
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}

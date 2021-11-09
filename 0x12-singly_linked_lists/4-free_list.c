a60fc05bf57e50ee705fda7325408ca1df95a8c4
root <3626@holbertonschool.com>
2021-11-08 19:46:25 -0800

    free a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *free_list - frees a list_t
+ *@head: ptr
+ */
+void free_list(list_t *head)
+{
+	list_t *a;
+
+	while (head)
+	a = head;
+	free(a->str);
+	free(a);
+	head = head->next;
+}

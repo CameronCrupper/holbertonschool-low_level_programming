d74a030b6115e58f2bdd03386145555545c12a3b
root <3626@holbertonschool.com>
2021-11-11 07:19:03 -0800

    test

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+*print_listint - print all elements
+*@h: pointer
+*Return: number of elements
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t a = 0;
+	const listint_t *ptr = h;
+
+	if (ptr == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		printf("%d\n", ptr->n);
+		a++;
+	}
+	ptr = ptr->next;
+
+	return (a);
+}

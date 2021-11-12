41ac969d89471a6a203bb998d3d8e6164dac163b
root <3626@holbertonschool.com>
2021-11-12 13:25:58 -0800

    set bit value to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,16 +6,18 @@
  *set_bit - set bit to 1
  *@n: pointer
  *@index: index
- *
+ *Return: 1 or -1
  *
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int a;
 
-
-	
-	a = (1 << n) | index;
+	if (index > (10 * sizeof(*n)) - 1)
+	{
+		return (-1);
+	}
+	*n = *n | (1 << index);
 	if (n & a)
 	{
 		printf("1");

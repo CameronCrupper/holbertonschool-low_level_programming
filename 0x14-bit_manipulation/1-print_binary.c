1719d29e1cb8d71fdd154790b107df2b6f304385
root <3626@holbertonschool.com>
2021-11-12 13:42:44 -0800

    print binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,9 +11,22 @@
  */
 void print_binary(unsigned long int n)
 {
+	int a;
+
 	if (n > 1)
 	{
 		print_binary(n >> 1);
 		_putchar((n & 1) + '0');
+	}
+	for (a = 1 << 10; a > 0; a >>= 1)
+	{
+	if (n & a)
+	{
+		_putchar(1);
+	}
+	else
+	{
+		_putchar(0);
+	}
 	}
 }

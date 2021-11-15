b6e5f0841457fb3a9c8af0a69612a466cfd489f6
root <3626@holbertonschool.com>
2021-11-15 12:44:30 -0800

    return number of bits you need to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *flip_bits - number of bit from one to another
+ *@n: Number
+ *@m: Number
+ *Return: number of bits you need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int a;
+	unsigned long int b;
+
+	if (!n)
+	{
+		return ('\0');
+	}
+	if (!m)
+	{
+		return ('\0');
+	}
+	a = n ^ m;
+	while (a > 0)
+	{
+		b = b + (a & 1);
+		a = a >> 1;
+	}
+return (b);
+}

9f75dacd4a42ef5aa4601145fb94dfef0599df48
root <3626@holbertonschool.com>
2021-11-15 12:53:12 -0800

    test

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -17,13 +17,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	}
 	*n = *n | (1 << index);
-	if (index)
-	{
-		printf("1");
-	}
-	else
-	{
-		printf("0");
-	}
+
 	return (1);
 }

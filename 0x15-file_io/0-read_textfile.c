293db9c45aad036855c19fb41844357fce821da0
root <3626@holbertonschool.com>
2021-11-17 12:52:17 -0800

    read txt and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include "main.h"
+/**
+*read_textfile - reads text and prints to POSIX STDO
+*@filename: files name
+*@letters: number of letters it should read and print
+*Return: number of letters it could read and print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fo, fr, fw;
+	char *a;
+
+	a = malloc(letters);
+
+	fo = open(filename, O_RDONLY);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	if (fo == -1)
+	{
+		return (0);
+	}
+	if (a == NULL)
+	{
+		return (0);
+	}
+	fr = read(STDOUT_FILENO, a, letters);
+	fw = write(fo, a, fr);
+	close(fo);
+	return (fw);
+}

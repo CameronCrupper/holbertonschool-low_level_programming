0d885579baff58bf7c2f54c2cf3ac190a368acfe
root <3626@holbertonschool.com>
2021-11-18 09:21:31 -0800

    test

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		return (-1);
+		text_content = "";
 	}
 	fo = open(filename, O_WRONLY | O_APPEND);
 	if (fo == -1)

9879d70eb2984e2c6986260c34f5e24869a3e58d
root <3626@holbertonschool.com>
2021-11-18 08:05:48 -0800

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,10 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		fo = open(filename, O_RDONLY | O_CREAT);
-	}
-	if (fo == -1)
-	{
-		return (-1);
+		return (1);
 	}
 	fo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fo == -1)

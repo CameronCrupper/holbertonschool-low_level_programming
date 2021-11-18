cfb0507a7aaeffeff3a55332fb235dfc435ad5f7
root <3626@holbertonschool.com>
2021-11-17 20:39:27 -0800

    copy one file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <string.h>
+#include "main.h"
+/**
+*main - copy from one file to another
+*@argc: number of arguments
+*@argv:	name of files
+*Return: nothing else
+*
+*/
+int main(int argc, char *argv[])
+{
+	int f1, f2, fw, fc, a;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f1 = open(argv[1], O_RDONLY);
+	f2 = open(argv[2], O_RDONLY | O_CREAT | O_TRUNC, 0664);
+	while (a == 1024)
+	{
+		a = read(f1, buf, 1024);
+		if (a == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		fw = write(f2, buf, a);
+		if (fw == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	fc = close(f1);
+	if (fw == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
+		exit(100);
+	}
+	fc = close(f2);
+	if (fc == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
+		exit(100);
+	}
+return (1);
+}

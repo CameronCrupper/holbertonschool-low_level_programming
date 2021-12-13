d75e28b2f77d0ab7f793fa2d8de01e1a7af68550
root <3626@holbertonschool.com>
2021-12-12 18:51:13 -0800

    test

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,8 +7,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
+	unsigned long int a;
 
-	index = size % hash_djb2(key);
-	return (index);
+	a = hash_djb2(key) % size;
+	return (a);
 }

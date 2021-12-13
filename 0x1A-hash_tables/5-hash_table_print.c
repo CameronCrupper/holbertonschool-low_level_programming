3ce1e640cc32001aec7c98ccce099e471d39daa8
root <3626@holbertonschool.com>
2021-12-12 18:57:03 -0800

    test

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,5 +6,26 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int a;
+	int flag = 0;
+	hash_node_t *NN;
 
+	printf("{");
+	for (a = 0; a < ht->size; a++)
+	{
+		NN = ht->array[a];
+		while (NN != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", NN->key, NN->value);
+			flag = 1;
+			NN = NN->next;
+		}
+	}
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("}\n");
 }
